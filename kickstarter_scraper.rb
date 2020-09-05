af8d34f1260ac9132d697c10fec34adfe25ebf4e
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-05 21:52:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,6 +3,7 @@
 require "nokogiri"
 require "pry"
  
+<<<<<<< HEAD
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
@@ -29,3 +30,12 @@ def create_project_hash
   # return the projects hash
   projects
 end
+=======
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
+end
+ 
+create_project_hash
+>>>>>>> 26815fe72357a434092a60d0b9fbeaa1d14ddd44
